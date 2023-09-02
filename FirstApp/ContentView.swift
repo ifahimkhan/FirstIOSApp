0b061adbbbf9e6d9a6cae6e8a090dee82f65458b
Fahim Khan <fahim.khan@tagitglobal.com>
2023-09-02 19:29:15 +0400

    Weather App Screen Layout

--- FirstApp/ContentView.swift
+++ FirstApp/ContentView.swift
@@ -10,18 +10,71 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack{
-            Text("Hello World!")
-                .foregroundColor(Color.red)
-                .padding()
-            Text("Hello World!")
-                .foregroundColor(Color.red)
-                .padding()
-            Text("Hello World!")
-                .foregroundColor(Color.red)
-                .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            Text("Weather!")
+                .font(Font.largeTitle)
+                .foregroundColor(Color.gray)
+                .fontWeight(.thin)
+ 
+            HStack{
+                Image(systemName: "sun.max.fill")
+                    .foregroundColor(Color.orange)
+                Text("Monday")
+                    .foregroundColor(Color.black)
+                    .font(.body)
+                    .fontWeight(.thin)
+            }.padding(.all,4.0)
+            HStack{
+                Image(systemName: "cloud.sun.fill")
+                    .foregroundColor(Color.orange)
+                Text("Tuesday")
+                    .foregroundColor(Color.black)
+                    .font(.body)
+                    .fontWeight(.thin)
+            }.padding(.all,4.0)
+            HStack{
+                Image(systemName: "cloud.sun.rain.fill")
+                    .foregroundColor(Color.yellow)
+                Text("Wednesday")
+                    .foregroundColor(Color.black)
+                    .font(.body)
+                    .fontWeight(.thin)
+            }.padding(.all,4.0)
+            HStack{
+                Image(systemName: "cloud.fill")
+                    .foregroundColor(Color.blue)
+                Text("Thursday")
+                    .foregroundColor(Color.black)
+                    .font(.body)
+                    .fontWeight(.thin)
+            }.padding(.all,4.0)
+            HStack{
+                Image(systemName: "smoke.fill")
+                    .foregroundColor(Color.blue)
+                Text("Friday")
+                    .foregroundColor(Color.black)
+                    .font(.body)
+                    .fontWeight(.thin)
+            }.padding(.all,4.0)
+            HStack{
+                Image(systemName: "cloud.drizzle.fill")
+                    .foregroundColor(Color.blue)
+                Text("Saturday")
+                    .foregroundColor(Color.black)
+                    .font(.body)
+                    .fontWeight(.thin)
+            }.padding(.all,4.0)
+            HStack{
+                Image(systemName: "cloud.bolt.rain.fill")
+                    .foregroundColor(Color.blue)
+                Text("Sunday")
+                    .foregroundColor(Color.black)
+                    .font(.body)
+                    .fontWeight(.thin)
+            }.padding(.all,4.0)
+          
         }
         
-            
+        
     }
 }
 
